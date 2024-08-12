7975b479a1c0d014d8e9912173a49af7c0f444dc
Jason Korol <ejkorol@gmail.com>
2024-08-12 18:22:00 -0400

    update gitignore for hidden files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./theme/**/*.liquid"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'lato': ['Lato', 'sans-serif'],
+        'americana': ['Americana', 'serif']
+      },
+      fontWeight: {
+        hairline: 100,
+        thin: 200,
+        light: 300,
+        regular: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        heavy: 800,
+      }
+    },
   },
   plugins: [],
 }
